a8f268995cc12ae3dde17d7fdd9b5ef70db30b66
Andrei-Levchuk <andreylevchuk3@gmail.com>
2024-04-18 17:46:36 +0200

    django_sprint4

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,39 +1,71 @@
 from django.contrib import admin
-
-from .models import Post, Location, Category
-
-
-class PostInline(admin.StackedInline):
-    model = Post
-    extra = 0
+from django.utils.safestring import mark_safe
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.http import urlencode
 
 
-class CategoryAdmin(admin.ModelAdmin):
-    inlines = (
-        PostInline,
-    )
+from .models import Category, Location, Post, Comment
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = (
-        'title',
-        'text'
-        'pub_date',
-        'author',
-        'location',
-        'is_published',
-        'created_at',
-        'category'
+        "title",
+        "preview",
+        "is_published",
+        "location",
+        "category",
+        "author",
+        "all_comments",
     )
-    list_editable = (
-        'is_published',
-        'category'
+    list_editable = ("is_published",)
+    search_fields = ("title",)
+    list_filter = (
+        "category",
+        "location",
     )
-    search_fields = ('title',)
-    list_filter = ('is_published',)
-    list_display_links = ('title',)
+    list_display_links = ("title",)
+    readonly_fields = ["preview"]
+
+    @admin.display(description="Миниатюра", empty_value="Нет фото")
+    def preview(self, object):
+        if object.image:
+            return mark_safe(
+                f'<img src="{object.image.url}" style="max-height: 100px;">'
+            )
+
+    @admin.display(description="Комментарии", empty_value="Нет комментариев")
+    def all_comments(self, object):
+        count = object.comments.count()
+        if count:
+            url = (
+                reverse("admin:blog_comment_changelist")
+                + "?"
+                + urlencode({"post__id": f"{object.id}"})
+            )
+            return format_html(
+                '<a href="{}"> Комментариев: ({})</a>', url, count
+            )
 
 
-admin.site.register(Post)
-admin.site.register(Location)
-admin.site.register(Category)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("text", "author")
+    search_fields = ("text__startswith",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "description",
+        "slug",
+        "is_published",
+    )
+    list_editable = ("is_published",)
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ("name",)
--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,37 +1,88 @@
-from django.db import models
 from django.contrib.auth import get_user_model
-
+from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 User = get_user_model()
 
-MAX_TITLE_LENGTH = 15
-MAX_LINE_LENGHT = 256
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super()
+            .get_queryset()
+            .filter(
+                is_published=True,
+                category__is_published=True,
+                pub_date__lte=timezone.now(),
+            )
+            .order_by('-pub_date')
+        )
 
 
-class PublishedModel(models.Model):
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name='Добавлено'
+    )
     is_published = models.BooleanField(
         default=True,
         verbose_name='Опубликовано',
-        help_text='Снимите галочку, чтобы скрыть публикацию.')
-    created_at = models.DateTimeField(
-        auto_now_add=True, verbose_name='Добавлено')
+        help_text='Снимите галочку, чтобы скрыть публикацию.',
+    )
 
     class Meta:
         abstract = True
 
 
-class Category(PublishedModel):
-    title = models.CharField(
-        max_length=MAX_LINE_LENGHT,
-        verbose_name='Заголовок'
+class Post(BaseModel):
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время публикации',
+        help_text='Если установить дату и время в будущем'
+        ' — можно делать отложенные публикации.',
+        default=timezone.now(),
     )
+    image = models.ImageField('Фото', upload_to='post_images', blank=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Автор публикации'
+    )
+    location = models.ForeignKey(
+        'Location',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='Местоположение',
+    )
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Категория',
+    )
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', kwargs={'pk': self.pk})
+
+
+class Category(BaseModel):
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
     description = models.TextField(verbose_name='Описание')
     slug = models.SlugField(
         unique=True,
         verbose_name='Идентификатор',
-        help_text=('Идентификатор страницы для URL; '
-                   'разрешены символы латиницы, цифры, '
-                   'дефис и подчёркивание.')
+        help_text='Идентификатор страницы для URL;'
+        ' разрешены символы латиницы, цифры, дефис и подчёркивание.',
     )
 
     class Meta:
@@ -39,55 +90,34 @@ class Category(PublishedModel):
         verbose_name_plural = 'Категории'
 
     def __str__(self):
-        return self.title[:MAX_TITLE_LENGTH]
+        return self.title
 
 
-class Location(PublishedModel):
-    name = models.CharField(
-        max_length=MAX_LINE_LENGHT,
-        verbose_name='Название места'
-    )
+class Location(BaseModel):
+    name = models.CharField(max_length=256, verbose_name='Название места')
 
     class Meta:
         verbose_name = 'местоположение'
         verbose_name_plural = 'Местоположения'
 
     def __str__(self):
-        return self.title[:MAX_TITLE_LENGTH]
+        return self.name
 
 
-class Post(PublishedModel):
-    title = models.CharField(
-        max_length=MAX_LINE_LENGHT,
-        verbose_name='Заголовок'
-    )
-    text = models.TextField(verbose_name='Текст')
-    pub_date = models.DateTimeField(
-        verbose_name='Дата и время публикации',
-        help_text=('Если установить дату и время в будущем — '
-                   'можно делать отложенные публикации.')
-    )
-    author = models.ForeignKey(
-        User,
+class Comment(models.Model):
+    text = models.TextField('Текст комментария')
+    post = models.ForeignKey(
+        Post,
         on_delete=models.CASCADE,
-        related_name='posts',
-        verbose_name='Автор публикации')
-    location = models.ForeignKey(
-        Location,
-        on_delete=models.SET_NULL, blank=True, null=True,
-        related_name='posts',
-        verbose_name='Местоположение'
+        related_name='comments',
     )
-    category = models.ForeignKey(
-        Category,
-        on_delete=models.SET_NULL, null=True,
-        related_name='posts',
-        verbose_name='Категория'
-    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name = 'публикация'
-        verbose_name_plural = 'Публикации'
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ('created_at',)
 
     def __str__(self):
-        return self.title[:MAX_TITLE_LENGTH]
+        return self.text
